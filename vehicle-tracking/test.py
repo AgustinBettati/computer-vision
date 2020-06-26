27213a5cc8e2d8ac6669dfd89ddc5c46dcc14f19
Agustin Bettati <bettatiagustin@gmail.com>
2020-06-26 19:16:05 -0300

    separate images for capturing contours

--- vehicle-tracking/test.py
+++ vehicle-tracking/test.py
@@ -22,10 +22,10 @@ old_car_imgs = []
 frame_counter = 0
 check_velocity = False
 
-def obtain_color(area):
-  if area < 2200:
+def obtain_color(area, is_top):
+  if (is_top and area < 1300) or (not is_top and area < 1500):
     return (0, 255, 0)
-  if area < 3000:
+  if (is_top and area < 2500) or (not is_top and area < 3000):
     return (0,255,255)
   else:
     return (0, 0, 255)
@@ -76,13 +76,28 @@ def obtain_center(contour):
   cY = int(M["m01"] / M["m00"])
   return [cX, cY]
 
+p1 = (0,190)
+p2 = (640,174)
+def mutate_y_value(val, flip):
+  (y_index, x_array) = val
+  for x_val in enumerate(x_array):
+    (x_index, value) = x_val
+    if flip and y_index > 190:
+      x_array[x_index] = 0
+    elif not flip and y_index < 174:
+      x_array[x_index] = 0
+    else:
+      y_result = -0.025 * x_index + 190
+      if flip and y_index > y_result:
+        x_array[x_index] = 0
+      if not flip and y_index < y_result:
+        x_array[x_index] = 0
 
 while (cap.isOpened):
-
   import time
-  time.sleep(0.100)
-  # if ret is true than no error with cap.isOpened
+  # time.sleep(0.100)
   ret, frame = cap.read()
+  clean_frame = frame.copy()
 
   frame_counter = frame_counter + 1
   if (frame_counter > 3):  # cada 0.1s
@@ -96,23 +111,37 @@ while (cap.isOpened):
   fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel) # Dilation followed by Erosion
   fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel) # Erosion followed by dilation
 
-  # check opencv version
+  top_half = fgmask.copy()
+  for val in enumerate(top_half):
+    mutate_y_value(val, True)
 
-  (contours, hierarchy) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  bottom_half = fgmask.copy()
+  for val in enumerate(bottom_half):
+    mutate_y_value(val, False)
 
-  filtered_contours = list(filter(lambda c: cv2.contourArea(c) > 500, contours))
-  car_imgs = list(map(lambda c: contour_to_img(frame, c), filtered_contours))
+  (top_contours, hierarchy) = cv2.findContours(top_half, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  (bottom_contours, hierarchy) = cv2.findContours(bottom_half, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-  cv2.imshow('live cars', mosaic_view(car_imgs))
+  def valid_countor(c):
+    (x, y, width, h) = cv2.boundingRect(c)
+    return (cv2.contourArea(c) > 500 and width < 150)
+  filtered_contours_top = list(filter(lambda c: valid_countor(c), top_contours))
+  filtered_contours_bottom = list(filter(lambda c: valid_countor(c), bottom_contours))
+
+  car_imgs = list(map(lambda c: contour_to_img(frame, c), filtered_contours_bottom + filtered_contours_top))
+
+  if (len(car_imgs) != 0):
+    cv2.imshow('live cars', mosaic_view(car_imgs))
   if len(old_car_imgs) != 0:
     cv2.imshow('historic', mosaic_view(old_car_imgs))
 
   for car in car_list:
     car.updated = False
 
-  centers = list(map(obtain_center, filtered_contours))
+  filtered_contours = list(map(lambda x: (x, True), filtered_contours_top)) + list(map(lambda x: (x, False), filtered_contours_bottom))
+
   # looping for contours
-  for c in filtered_contours:
+  for (c, is_top) in filtered_contours:
     M = cv2.moments(c)
     cX = int(M["m10"] / M["m00"])
     cY = int(M["m01"] / M["m00"])
@@ -130,12 +159,18 @@ while (cap.isOpened):
         if(check_velocity):
           if(car.hist_x != 0 and car.hist_y != 0):
             distance = calculateDistance(cX, cY, car.hist_x, car.hist_y)
-            distanceM = distance * 0.07 # en 0.1s
+            global distanceM # en 0.1s
+            if is_top:
+              distanceM = distance * 0.102
+            else:
+              distanceM = distance * 0.07
             meterPerSecond = distanceM / 0.1
             velocity = int(meterPerSecond * 3.6)
             car.velocity = velocity
-            car_img = contour_to_img(frame, c)
-            car.img = car_img
+            car_img = contour_to_img(clean_frame, c)
+            height, width, channels = car_img.shape
+            if(width > 50 and height < 70):
+              car.img = car_img
           car.hist_x = cX
           car.hist_y = cY
         else:
@@ -149,7 +184,7 @@ while (cap.isOpened):
     (x, y, w, h) = cv2.boundingRect(c)
 
     # draw bounding box
-    cv2.rectangle(frame, (x, y), (x + w, y + h), obtain_color(cv2.contourArea(c)), 2)
+    cv2.rectangle(frame, (x, y), (x + w, y + h), obtain_color(cv2.contourArea(c), is_top), 2)
 
     cv2.putText(frame, str(velocity) + " km/h", (x, y - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
 
@@ -167,6 +202,7 @@ while (cap.isOpened):
   car_list = list(filter( lambda c: c.inactive_counter < 5, car_list))
 
   # cv2.imshow('foreground and background', fgmask)
+  # cv2.line(frame, p1, p2, (0, 255, 0), thickness=2)
   check_velocity = False
   cv2.imshow('rgb', frame)
   if cv2.waitKey(1) & 0xFF == ord("q"):
