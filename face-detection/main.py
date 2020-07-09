da6e568f9487455fdb3f269a38685da85cffe83b
Agustin Bettati <bettatiagustin@gmail.com>
2020-07-09 17:41:20 -0300

    resize of image for displaying and processing

--- face-detection/main.py
+++ face-detection/main.py
@@ -1,6 +1,7 @@
 import sys
 import face_recognition
 import cv2
+import imutils as imutils
 import numpy as np
 import yaml
 from mosaic_view import mosaic_view, concat_face_imgs
@@ -10,6 +11,8 @@ from person import Person
 # - mostrar imagen de perfil y una del video
 
 video_path = "/Users/abettati/Desktop/videos-vision/Screen Recording 2020-07-02 at 8.34.21 PM.mov"
+# video_path = "/Users/abettati/Desktop/grande-detecta-bien.mov"
+# video_path = "/Users/abettati/Desktop/no-detecta-nada.mov"
 video_capture = cv2.VideoCapture(video_path)
 
 # Load a sample picture and learn how to recognize it.
@@ -44,8 +47,10 @@ while (video_capture.isOpened):
     ret, frame = video_capture.read()
 
     if not frame is None:
+
+        h, w = frame.shape[:2]
         # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        small_frame = imutils.resize(frame, width=1000)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]
@@ -53,6 +58,7 @@ while (video_capture.isOpened):
         # Only process every other frame of video to save time
         if process_this_frame:
             # Find all the faces and face encodings in the current frame of video
+            # face_locations = face_recognition.face_locations(rgb_small_frame, number_of_times_to_upsample=0, model="cnn")
             face_locations = face_recognition.face_locations(rgb_small_frame)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
@@ -62,16 +68,16 @@ while (video_capture.isOpened):
                 face_location = face_locations[i]
                 name = "Unknown"
                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding, 0.55)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding, 0.50)
 
                 best_match_index = np.argmin(face_distances)
                 if matches[best_match_index]:
                     participant = participants[best_match_index]
                     participant.is_present = True
                     name = participant.name
-                    (top, right, bottom, left) = list(map(lambda x: x * 4, face_locations[i]))
-                    margin = 10
-                    video_image = frame[top -margin: bottom + margin, left - margin:right + margin]
+                    (top, right, bottom, left) = face_locations[i]
+                    margin = 35
+                    video_image = small_frame[top -margin: bottom + margin, left - margin:right + margin]
                     if len(participant.video_img) == 0:
                       participant.video_img = video_image.copy()
 
@@ -82,22 +88,18 @@ while (video_capture.isOpened):
 
         # Display the results
         for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
 
             # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            margin = 20
+            cv2.rectangle(small_frame, (left - margin, top - margin), (right + margin, bottom + margin), (0, 0, 255), 2)
 
             # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            cv2.rectangle(small_frame, (left - margin, bottom + margin - 20), (right + margin, bottom + margin), (0, 0, 255), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.8, (255, 255, 255), 1)
+            cv2.putText(small_frame, name, (left - margin + 6, bottom + margin - 6), font, 0.5, (255, 255, 255), 1)
 
         # Display the resulting image
-        cv2.imshow('Video', frame)
+        cv2.imshow('Video', small_frame)
     else:
       break
     # Hit 'q' on the keyboard to quit!
@@ -107,8 +109,12 @@ while (video_capture.isOpened):
 present = list(filter(lambda p: p.is_present, participants))
 absent = list(filter(lambda p: not p.is_present, participants))
 
-cv2.imwrite('./output/present.jpg', mosaic_view(list(map(lambda p: (concat_face_imgs(p), p), present))))
-cv2.imwrite('./output/absent.jpg', mosaic_view(list(map(lambda p: (p.img, p), absent))))
+present_img = mosaic_view(list(map(lambda p: (concat_face_imgs(p), p), present)))
+absent_img = mosaic_view(list(map(lambda p: (p.img, p), absent)))
+if not present_img is None:
+  cv2.imwrite('./output/present.jpg', present_img)
+if not absent_img is None:
+  cv2.imwrite('./output/absent.jpg', absent_img)
 
 # Release handle to the webcam
 with open('./output/asistencia.yaml', 'w') as outfile:
